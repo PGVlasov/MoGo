2595abb3cba68f48571971e7d7ffdece95dee82b
PGVlasov <PGVlasov@users.noreply.github.com>
2022-07-07 20:41:19 +0300

    ready

---
+++ .history/test_20220706121834.js
@@ -0,0 +1,37 @@
+let courses = [
+  { name: "Courses in England", prices: [0, 100] },
+  { name: "Courses in Germany", prices: [500, null] },
+  { name: "Courses in Italy", prices: [100, 200] },
+  { name: "Courses in Russia", prices: [null, 400] },
+  { name: "Courses in China", prices: [50, 250] },
+  { name: "Courses in USA", prices: [200, null] },
+  { name: "Courses in Kazakhstan", prices: [56, 324] },
+  { name: "Courses in France", prices: [null, null] },
+];
+
+//let from = courses[i].prices[0];
+let from = courses.map((course) => course.prices[0]);
+console.log("FROM", from);
+
+//let to = courses[i].prices[1];
+
+let to = courses.map((course) => course.prices[1]);
+
+console.log("TO", to);
+
+const sortCurses = ([from, to]) => {
+  for (let i = 0; i < courses.length; i++) {
+    if ((from === null || from === 0) && to <= 200) {
+      console.log(to);
+    }
+  }
+};
+
+// Варианты цен (фильтры), которые ищет пользователь
+let requiredRange1 = [null, 200];
+let requiredRange2 = [100, 350];
+let requiredRange3 = [200, null];
+
+sortCurses(requiredRange1);
+sortCurses(requiredRange2);
+sortCurses(requiredRange3);
---
+++ .history/test_20220706122453.js
@@ -0,0 +1,35 @@
+let courses = [
+  { name: "Courses in England", prices: [0, 100] },
+  { name: "Courses in Germany", prices: [500, null] },
+  { name: "Courses in Italy", prices: [100, 200] },
+  { name: "Courses in Russia", prices: [null, 400] },
+  { name: "Courses in China", prices: [50, 250] },
+  { name: "Courses in USA", prices: [200, null] },
+  { name: "Courses in Kazakhstan", prices: [56, 324] },
+  { name: "Courses in France", prices: [null, null] },
+];
+
+let from = courses.map((course) => course.prices[0]);
+console.log("FROM", from);
+
+let to = courses.map((course) => course.prices[1]);
+
+console.log("TO", to);
+
+const sortCurses = ([from, to]) => {
+  for (let i = 0; i < courses.length; i++) {
+    // if ((from === null || from === 0) && to <= 200) {
+    //   console.log(courses[i]);
+    // }
+    console.log(courses[i].from);
+  }
+};
+
+// Варианты цен (фильтры), которые ищет пользователь
+let requiredRange1 = [null, 200];
+let requiredRange2 = [100, 350];
+let requiredRange3 = [200, null];
+
+sortCurses(requiredRange1);
+sortCurses(requiredRange2);
+sortCurses(requiredRange3);
